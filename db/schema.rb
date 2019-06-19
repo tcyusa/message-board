a6bfe97362c53d85431d61390fb10eef2b1ca107
tcyu.sa0618 <tcyu.sa0618@gmail.com>
2019-06-19 05:48:01 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_043822) do
+ActiveRecord::Schema.define(version: 2019_06_19_054207) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
